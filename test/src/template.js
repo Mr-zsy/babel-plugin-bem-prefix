ad2ed8f66d554d147d036fed8db53cd65230f8e0
zsy <zhaisiyuan@fenbi.com>
2021-09-01 19:16:33 +0800

    init

---
+++ test/src/template.js
@@ -0,0 +1,27 @@
+ // commentPrefix:  xxxx
+import './App.css';
+
+
+ // commentPrefix: xxxx1
+  /// xxx
+
+
+function App() {
+  const aaa = function() {
+    return <div className="&__test2222"></div>
+  }
+  return (
+    <div className="&__test"></div>
+  );
+}
+
+export default App;
+
+class Test extends React.Component {
+  // wwww
+  render() {
+    return (
+      <div className="&__ahh"></div>
+    )
+  }
+}
